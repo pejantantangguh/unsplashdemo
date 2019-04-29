1187a173c1915da5ce73d5a10fefbb5a178d311f
Pejantantangguh <suherman.ng@gmail.com>
2019-04-29 15:54:35 +1000

    Details page

--- src/Components/ImageDetails.jsx
+++ src/Components/ImageDetails.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import Unsplash, { toJson } from 'unsplash-js';
+
+const unsplash = new Unsplash({
+    applicationId: "1e2735fc9fdf7638b606cfad6e52068ab6d1922b6cdfd7950d768c5d372e04c9",
+    secret: "cb6f12d85cb8aa9d500b8ba87e16c093a9d97ff781e877719dcf678735f83999"
+})
+
 class ImageDetails extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { image: [] };
+    }
+
+    componentDidMount() {
+        unsplash.photos.getPhoto(`${this.props.id}`)
+            .then(toJson)
+            .then(async json => {
+                let image = await json;
+                console.log(image)
+                this.setState({
+                    photographerName: image.user.name,
+                    imageName: image.alt_description,
+                    media: image.urls.regular,
+                    location: image.user.location
+                });
+            });
+    }
     render() {
+        const { photographerName, imageName, media } = this.state;
         return (
             <div>
-                <h1>Hello World</h1>
+                <div className="mw5 mw7-ns center bg-light-gray pa3 ph5-ns dim">
+                    <img src={media} alt={imageName} className="mt0" />
+                </div >
             </div>
         )
     }
